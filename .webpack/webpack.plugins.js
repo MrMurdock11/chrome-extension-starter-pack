9e5be3d9a0f63d67acd39139879149fea771933d
MrMurdock11 <zikbest11@yandex.ru>
2021-02-28 18:31:50 +0300

    Добавил плагины для сборки.
    - Добавил плагины для сборки. Теперь папка для сборки очищается и в нее перемещаются необходимые для работы приложения компоненты.

--- .webpack/webpack.plugins.js
+++ .webpack/webpack.plugins.js
@@ -1,1 +1,9 @@
-module.exports = [];
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = [
+	new CleanWebpackPlugin(),
+	new CopyPlugin({
+		patterns: [{ from: "manifest.json" }, { from: "icons", to: "icons" }],
+	}),
+];
